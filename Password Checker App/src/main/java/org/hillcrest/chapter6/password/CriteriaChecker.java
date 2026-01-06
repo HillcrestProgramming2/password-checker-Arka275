7ccb58e6b8144aba712fdf87eaf08df9640e1ece
Arka275 <praarkahc@gmail.com>
2026-01-06 08:54:57 -0700

    ea
    e

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -2,5 +2,28 @@ package org.hillcrest.chapter6.password;
 
 public class CriteriaChecker {
 
-    //utility class
+    public static int evaluateCriteria(String password){
+        String lowerCaseLetters = "abcdefghijklmnopqrstuvwuxyz";
+        String upperCaseLetters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String number = "1234567890";
+        int score = 0;
+        boolean hasUpper = false;
+        if (password.length() >= 8) {
+            score++;
+            for(int i = 0;i < password.length(); i++){
+                if (upperCaseLetters.contains(password.substring(i, i+1))){
+                    hasUpper = true;
+                }else{
+
+                }
+            }
+        }
+        else {
+
+        }
+
+
+    }
+
+
 }
