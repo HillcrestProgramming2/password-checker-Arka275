15a05c2feca54e79793a9636e0c499e105906289
Arka275 <praarkahc@gmail.com>
2026-01-06 22:18:53 -0700

    efw

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -2,28 +2,65 @@ package org.hillcrest.chapter6.password;
 
 public class CriteriaChecker {
 
-    public static int evaluateCriteria(String password){
+    public static String evaluateCriteria(String password){
         String lowerCaseLetters = "abcdefghijklmnopqrstuvwuxyz";
         String upperCaseLetters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String number = "1234567890";
+        String special = "!@#$%^&*()-+=";
         int score = 0;
+        String strength = "";
         boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+
+
         if (password.length() >= 8) {
             score++;
-            for(int i = 0;i < password.length(); i++){
-                if (upperCaseLetters.contains(password.substring(i, i+1))){
+        }
+
+        for (int i = 0;i < password.length(); i++) {
+
+            if (upperCaseLetters.contains(password.substring(i, i+1))) {
                     hasUpper = true;
-                }else{
-
                 }
+            if (lowerCaseLetters.contains(password.substring(i, i+1))) {
+                hasLower = true;
             }
-        }
-        else {
+            if (number.contains(password.substring(i, i+1))) {
+                hasNumber = true;
+            }
+            if (special.contains(password.substring(i, i+1))) {
+                hasSpecial = true;
+            }
 
         }
 
+        if (hasUpper) {
+            score++;
+        }
+        if (hasLower) {
+            score++;
+        }
+        if (hasNumber) {
+            score++;
+        }
+        if (hasSpecial) {
+            score++;
+        }
+        if (score >= 0 && score <= 2){
+            strength = "Weak";
+        }
+        if (score == 3){
+            strength = "Moderate";
+        }
+        if (score == 4 || score == 5){
+            strength = "Strong";
+        }
 
-    }
+        return strength;
+
+        }
 
 
 }
